ad423c66930d743d67549a893b3045efbf993599
SnowSuno <percy3368@gmail.com>
2021-07-03 19:05:08 +0900

    feat : add header design

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./style.css"
+import {ReactComponent as Title} from "../../resource/img/logo/title.svg";
+import {ReactComponent as MenuIcon} from "../../resource/img/icon/menu.svg";
+import {ReactComponent as SettingsIcon} from "../../resource/img/icon/settings.svg";
 
 function Header() {
 
@@ -7,10 +10,14 @@ function Header() {
     return (
         <div className='header'>
             <button className='menu'>
+                <MenuIcon />
                 <span>MENU</span>
             </button>
-            <div className='title'>SYNCROOM KOREA</div>
-            <button className='settings'>설정</button>
+            <Title height='80px'/>
+            <button className='settings'>
+                <div>설정</div>
+                <SettingsIcon />
+            </button>
         </div>
     )
 }
