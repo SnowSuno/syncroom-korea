22b01872618dac0c96b11675e1719f53a344cc19
SnowSuno <percy3368@gmail.com>
2021-07-03 16:49:14 +0900

    feat : add fonts

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,11 @@ function Header() {
 
     return (
         <div className='header'>
-            이 부분은 야무진 헤더가 될 예정입니다ㅎㅎ
+            <div className='menu'>
+                <span>MENU</span>
+            </div>
+            <div className='title'>SYNCROOM KOREA</div>
+            <div className='settings'>설정</div>
         </div>
     )
 }
