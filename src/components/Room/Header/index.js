ec4ba0b46960af1e2de5cf86769ea56ecd1f974d
SnowSuno <percy3368@gmail.com>
2021-07-03 13:43:35 +0900

    feat : fix styling

--- src/components/Room/Header/index.js
+++ src/components/Room/Header/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./style.css";
 
-function RoomHeader({ room_name, need_passwd, num_members, country }) {
-    const color = need_passwd
-        ? (num_members === 5) ? '#4B4B4B' : '#000000'
-        : (num_members === 5) ? '#7680A2' : '#132F92';
+function RoomHeader({ room_name, need_passwd, num_members, country, color }) {
+
 
     return (
         <div className="room-header" style={{background: color}}>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,10 +4,15 @@ import "./Home.css";
 import useRoomData from "../hooks/useRoomData";
 import Room from "../components/Room/";
 
+import useModal from "../components/modals/useModal";
+import Modal from "../components/modals/Modal";
+
 
 
 function Home() {
     const { roomData, loading, error, fetchRooms } = useRoomData();
+
+    const [open, openModal, closeModal] = useModal();
 
     if (!loading) {
         if (!error) {
@@ -33,6 +38,12 @@ function Home() {
                 {roomData.map((room, index) => <Room key={index} {...room}/>)}
                 {/*{Array(2).fill(<div style={{width: '388px'}}/>)}*/}
             </div>
+
+            <Modal open={open} closeModal={closeModal} header="qwerqwer">
+                this is a modal
+            </Modal>
+            <button onClick={openModal}>open</button>
+
         </div>
     )
 }
