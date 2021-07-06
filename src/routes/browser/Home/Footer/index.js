1d8a003f4390228d793102a1168675d3ec8fb25b
SnowSuno <percy3368@gmail.com>
2021-07-07 02:26:59 +0900

    feat : change footer message

--- src/routes/browser/Home/Footer/index.js
+++ src/routes/browser/Home/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
         <div className="footer">
             <br/>
             SYNCROOM KOREA WEBSITE<br/>
-            WEBSITE : 권순호  |  WEB DESIGN : 박주원<br/>
+            WEB DEVELOPMENT : 권순호  |  WEB DESIGN : 박주원<br/>
             © 2021. 권순호, 박주원 all rights reserved.<br/>
             CONTACT US : kr.syncroom@gmail.com<br/>
             <br/>
