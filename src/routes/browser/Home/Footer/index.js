9448a93aeade69415439bf445387c024bf30ac31
SnowSuno <percy3368@gmail.com>
2021-07-07 02:21:10 +0900

    feat : add footer

--- src/routes/browser/Home/Footer/index.js
+++ src/routes/browser/Home/Footer/index.js
@@ -4,7 +4,12 @@ import "./style.css"
 function Footer() {
     return (
         <div className="footer">
-            this is a footer
+            <br/>
+            SYNCROOM KOREA WEBSITE<br/>
+            WEB DEVELOPMENT : 권순호  |  WEB DESIGN : 박주원<br/>
+            © 2021. 권순호, 박주원 all rights reserved.<br/>
+            CONTACT US : kr.syncroom@gmail.com<br/>
+            <br/>
         </div>
     )
 }
