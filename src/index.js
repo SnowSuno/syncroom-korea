94cf121a6377f404e444316ff80fd7173a56ba2b
SnowSuno <percy3368@gmail.com>
2021-07-09 23:11:17 +0900

    feat : create store and add provider

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import rootReducer from "./store";
+import ReduxThunk from "redux-thunk";
 
 import {BrowserView, MobileView} from "react-device-detect";
 import BrowserRoutes from "./routes/browser/Routes";
 import MobileRoutes from "./routes/mobile/Routes";
 
+const store = createStore(
+    rootReducer,
+    applyMiddleware(ReduxThunk)
+);
+
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserView>
-            <BrowserRoutes />
-        </BrowserView>
-        <MobileView>
-            <MobileRoutes />
-        </MobileView>
+        <Provider store={store}>
+            <BrowserView>
+                <BrowserRoutes />
+            </BrowserView>
+            <MobileView>
+                <MobileRoutes />
+            </MobileView>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
