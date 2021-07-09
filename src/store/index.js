8ef0513dfe84889927d6eb86bc32e7fa6da51eec
SnowSuno <percy3368@gmail.com>
2021-07-09 22:28:12 +0900

    feat : combine reducers into rootreducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import {combineReducers} from "redux";
 
+import rooms from "./rooms";
+import filter from "./filter";
 
-const rootReducer = combineReducers({
-
-});
+const rootReducer = combineReducers({rooms, filter});
 
 export default rootReducer;
