ab4c46ed4201f248d12e557f738a5f348bbb79c0
SnowSuno <percy3368@gmail.com>
2021-07-10 17:23:34 +0900

    feat : initialize api data handlers

---
+++ src/common/util/filterRooms.js
@@ -0,0 +1,21 @@
+import {country, inst, status} from "../../store/filter";
+
+
+const filterCountry = country => {
+
+
+    // 필터링해주는 함수를 리턴
+    return room => {
+
+    }
+}
+
+
+// rooms array와 필터 상태 객체를 받아서 필터된 방 array를 return하는 함수
+const filterRooms = (rooms, filter) => {
+    
+
+
+
+
+}
