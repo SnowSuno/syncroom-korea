e4754bfb481ff984fd205330624a7aaa21223148
SnowSuno <percy3368@gmail.com>
2021-07-18 19:53:53 +0900

    feat : add instrument profile icons

--- src/resource/img/icon/Profile/index.tsx
+++ src/resource/img/icon/Profile/index.tsx
@@ -1,21 +1,51 @@
 import React, {ReactElement} from "react";
 
+import {ReactComponent as Drums} from "./drums.svg";
+import {ReactComponent as Bongos} from "./bongos.svg";
 import {ReactComponent as Bass} from "./bass.svg";
+import {ReactComponent as Electric} from "./electric.svg";
+import {ReactComponent as Acoustic} from "./acoustic.svg";
+import {ReactComponent as Keyboard} from "./keyboard.svg";
+import {ReactComponent as Piano} from "./piano.svg";
+import {ReactComponent as Trumpet} from "./trumpet.svg";
+import {ReactComponent as Saxophone} from "./saxophone.svg";
+import {ReactComponent as Flute} from "./flute.svg";
+import {ReactComponent as Violin} from "./violin.svg";
+import {ReactComponent as DJ} from "./dj.svg";
+import {ReactComponent as Vocal} from "./vocal.svg";
+import {ReactComponent as Stage} from "./stage.svg";
 
 interface ProfileProps {
     icon: string
 }
 
 const iconMap: {[index: string]: ReactElement} = {
-    "1": <Bass />,
+    "0": <Drums />,
+    "1": <Bongos />,
+    "2": <Bass />,
+    "3": <Electric />,
+    "4": <Acoustic />,
+    "5": <Keyboard />,
+    "6": <Piano />,
+    "7": <Trumpet />,
+    "8": <Saxophone />,
+    "9": <Flute />,
+    "10": <Violin />,
+    "11": <DJ />,
+    "12": <Vocal />,
+    "13": <Stage />
 }
 
 function Profile({icon}: ProfileProps) {
-    if (icon in iconMap) {
-        return iconMap[icon];
-    } else {
-        return <img src={icon} alt=""/>
-    }
+    return (
+        <div className="icon">
+            {
+                icon in iconMap
+                    ? iconMap[icon]
+                    : <img src={icon} alt=""/>
+            }
+        </div>
+    )
 }
 
 export default Profile;
