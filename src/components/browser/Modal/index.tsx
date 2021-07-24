ae1344de10d0e2fd1202287800139d5ef87c2a0e
SnowSuno <percy3368@gmail.com>
2021-07-25 02:54:16 +0900

    fix : react modal initial setting fix

--- src/components/browser/Modal/index.tsx
+++ src/components/browser/Modal/index.tsx
@@ -1,20 +1,23 @@
 import ReactDOM from "react-dom";
-import React from "react";
+import React, {useState} from "react";
 
 import "./style.css"
 
 function Modal() {
     const modalElement: HTMLElement = document.getElementById("modal")!;
 
+
+
     return ReactDOM.createPortal(
         <>
-            {
-
-            }
+            <div>
+                hello im a modal
+            </div>
 
         </>,
         modalElement
     )
 }
+
 
 export default Modal;
