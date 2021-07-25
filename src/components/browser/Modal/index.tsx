9ca1e52aaa2cd533581070e54aacf9568db46246
SnowSuno <percy3368@gmail.com>
2021-07-25 16:20:03 +0900

    chore : modal styling

--- src/components/browser/Modal/index.tsx
+++ src/components/browser/Modal/index.tsx
@@ -5,6 +5,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../modules";
 import {closeModal} from "../../../modules/modal";
 
+import {ReactComponent as Lock} from "../../../resource/img/icon/lock.svg";
+
 function Modal() {
     const {isOpen, roomName} = useSelector((state: RootState) => state.modal);
     const dispatch = useDispatch();
@@ -23,6 +25,11 @@ function Modal() {
             ? <>
                 <div className={`overlay ${fade}`} onClick={close} />
                 <div className={`modal ${fade}`}>
+                    <div className="title">
+                        <Lock fill="#000000"/>
+                        {roomName}
+                    </div>
+
                     hello im a open modal
                     <div>
                         {roomName}
