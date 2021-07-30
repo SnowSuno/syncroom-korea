9d8c5bd5403688e5d9e3e08ca3f6744580497899
SnowSuno <percy3368@gmail.com>
2021-07-30 19:30:47 +0900

    chore : modals styling

--- src/components/browser/Modal/ShareModal.tsx
+++ src/components/browser/Modal/ShareModal.tsx
@@ -9,6 +9,8 @@ import {Status} from "../../../common/classes/types";
 
 import {shareLinkDomain, encodeShareLink} from "../../../common/util/shareLink";
 
+import {CopyToClipboard} from "react-copy-to-clipboard";
+
 import {ReactComponent as Link} from "../../../resource/img/icon/link.svg";
 
 
@@ -28,10 +30,7 @@ function ShareModal() {
         {roomName, password: password.value}
     );
     const handleFocus = (event: React.ChangeEvent<HTMLInputElement>) => event.target.select();
-    const copy = () => {
-        linkField.current?.select();
-        document.execCommand('copy');
-    };
+    const [copied, setCopied] = useState(false);
 
     return (
         <div className="share modal">
@@ -46,19 +45,33 @@ function ShareModal() {
                     onFocus={handleFocus}
                     ref={linkField}
                 />
-                <button
-                    onClick={copy}
-                    disabled={(checked && (password.value.length === 0))}
-                >
-                    <span>복사</span>
-                </button>
-            </div>
+                <CopyToClipboard text={shareLinkDomain + shareLink}>
+                    <button
+                        onClick={() => setCopied(true)}
+                        disabled={(checked && (password.value.length === 0))}
+                    >
+                        <span>복사</span>
+                    </button>
+                </CopyToClipboard>
 
+            </div>
+            <div className={`copied ${copied ? '' : 'hide'}`}>
+                <svg viewBox="0 0 21 21" height="0.8rem">
+                    <circle cx="10.5" cy="10.5" r="10.5" fill="#49C5B6"/>
+                    <polyline
+                        points="5 10.75 8.5 14.25 16 6"
+                        stroke="#FFFFFF"
+                        strokeWidth="0.2rem"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        fill="none"
+                    />
+                </svg>
+                클립보드에 복사되었습니다
+            </div>
             {status === Status.PRIVATE
                 ? <div>
                     <div className="checkbox" onClick={onClick}>
-                        {/*<input type="checkbox" id="password"/>*/}
-                        {/*<label htmlFor="password">비밀번호 포함하기</label>*/}
                         <div className={`box ${checkedClass}`}>
                             <svg viewBox="0 0 21 21">
                                 <polyline points="5 10.75 8.5 14.25 16 6"/>
@@ -75,7 +88,6 @@ function ShareModal() {
                         />
                     </div>
                 </div>
-
                 : <div />
             }
         </div>
