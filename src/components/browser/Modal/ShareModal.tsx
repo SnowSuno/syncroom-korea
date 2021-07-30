ce0886d70affb4a16b5f065e37342f3420ece7ec
SnowSuno <percy3368@gmail.com>
2021-07-30 13:31:50 +0900

    feat : add functionality to share modal

--- src/components/browser/Modal/ShareModal.tsx
+++ src/components/browser/Modal/ShareModal.tsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ShareModal.css";
 
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {RootState} from "../../../modules";
 import useInput from "../../../common/hooks/useInput";
-import {closeModal} from "../../../modules/modal";
 
-import {encodeShareLink} from "../../../common/util/shareLink";
+import {Status} from "../../../common/classes/types";
+
+import {shareLinkDomain, encodeShareLink} from "../../../common/util/shareLink";
 
 import {ReactComponent as Link} from "../../../resource/img/icon/link.svg";
 
 
 function ShareModal() {
     const {roomName, status} = useSelector((state: RootState) => state.modal);
-    // const dispatch = useDispatch();
+
     const password = useInput('');
-    // const close = () => dispatch(closeModal());
+    const [checked, setChecked] = useState(status === Status.PRIVATE);
+    const checkedClass = checked ? 'checked' : 'unchecked';
+    const onClick = () => {
+        password.set('');
+        setChecked(!checked);
+    };
 
     const shareLink: string = encodeShareLink(
-        {roomName, password: password.value});
+        {roomName, password: password.value}
+    );
+    const handleFocus = (event: React.ChangeEvent<HTMLInputElement>) => event.target.select();
 
     return (
         <div className="share modal">
@@ -27,20 +35,42 @@ function ShareModal() {
                 <span>{roomName}</span>
             </div>
             <div className="link">
-                <input type="text"/>
-                <button>
+                <input
+                    type="text"
+                    value={shareLinkDomain + shareLink}
+                    onFocus={handleFocus}
+                />
+                <button
+                    disabled={(checked && (password.value.length === 0))}
+                >
                     <span>복사</span>
                 </button>
             </div>
 
-
-            <div>
-                <div className="checkbox">
-                    <div className="box"/>
-                    <span className="desc">비밀번호 포함하기</span>
+            {status === Status.PRIVATE
+                ? <div>
+                    <div className="checkbox" onClick={onClick}>
+                        {/*<input type="checkbox" id="password"/>*/}
+                        {/*<label htmlFor="password">비밀번호 포함하기</label>*/}
+                        <div className={`box ${checkedClass}`}>
+                            <svg viewBox="0 0 21 21">
+                                <polyline points="5 10.75 8.5 14.25 16 6"/>
+                            </svg>
+                        </div>
+                        <span className="desc">비밀번호 포함하기</span>
+                    </div>
+                    <div className={`password ${checkedClass}`}>
+                        <input
+                            type="text"
+                            placeholder="비밀번호를 입력하세요"
+                            disabled={!checked}
+                            {...password}
+                        />
+                    </div>
                 </div>
-                <input type="text" placeholder="비밀번호를 입력하세요"/>
-            </div>
+
+                : <div />
+            }
         </div>
     );
 }
