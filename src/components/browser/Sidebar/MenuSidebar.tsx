1555f963ba5c4e047645026f6ca5273f4f9e583f
SnowSuno <percy3368@gmail.com>
2021-08-21 18:11:49 +0900

    feat : menu sidebar styling

--- src/components/browser/Sidebar/MenuSidebar.tsx
+++ src/components/browser/Sidebar/MenuSidebar.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "./MenuSidebar.css";
 
+import {closeSidebar} from "../../../modules/sidebar";
+import {useDispatch} from "react-redux";
+
+
+import {ReactComponent as Logo} from "../../../resource/img/logo/title.svg";
 
 
 function MenuSidebar() {
-
+    const dispatch = useDispatch();
+    const close = () => dispatch(closeSidebar());
 
     return (
         <div className="menu sidebar">
+            <div className="header">
+                <button onClick={close}>
+                    -
+                </button>
+                <Logo />
+            </div>
+
 
         </div>
     );
