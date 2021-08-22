f56d796120151d2268870a478747a03195a25340
SnowSuno <percy3368@gmail.com>
2021-08-22 11:39:54 +0900

    feat : beta modal styling

--- src/components/temp/BetaNotiModal.tsx
+++ src/components/temp/BetaNotiModal.tsx
@@ -2,6 +2,7 @@
 //
 
 import React, {useState} from "react";
+import "./BetaNotiModal.css";
 
 
 function BetaNotiModal() {
@@ -12,7 +13,10 @@ function BetaNotiModal() {
             ? <div id="beta-noti-modal">
                 <div className="overlay" onClick={() => setOpen(false)}/>
                 <div className="temp-modal">
-
+                    <div className="title">SYNCROOM KOREA BETA TEST에 오신 것을 환영합니다!</div>
+                    Syncroom Korea는 한국 사용자들이 더 편하게 싱크룸 방 입장 페이지를 이용할 수 있도록 만들어진 웹사이트입니다.
+                    <br/>
+                    (대충 공지 내용 적을 예정)
                 </div>
             </div>
             : <></>
