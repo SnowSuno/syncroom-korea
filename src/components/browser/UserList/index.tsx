2b14259c620e15cd93bc3c2b4a78bb363fb9faf0
SnowSuno <percy3368@gmail.com>
2021-09-03 03:53:01 +0900

    chore : delete unused lines

--- src/components/browser/UserList/index.tsx
+++ src/components/browser/UserList/index.tsx
@@ -35,22 +35,6 @@ function UserList() {
             {add: isAdd}
         )}>
             <Manage {...{isActive, handleActive, isAdd, handleAdd}}/>
-            {/*<div className="users">*/}
-            {/*    <div className="status-tag">온라인 ― {onlineUsers.length}</div>*/}
-            {/*    <div>*/}
-            {/*        {onlineUsers.map((userName) => (*/}
-            {/*            <OnlineUser userName={userName} roomId={users[userName]}/>*/}
-            {/*        ))}*/}
-            {/*    </div>*/}
-
-            {/*    <div className="status-tag">오프라인 ― {offlineUsers.length}</div>*/}
-            {/*    <div>*/}
-            {/*        {offlineUsers.map((userName) => (*/}
-            {/*            <OfflineUser userName={userName}/>*/}
-            {/*        ))}*/}
-            {/*    </div>*/}
-            {/*    <div className="padding" />*/}
-            {/*</div>*/}
 
             <SimpleBar className="users">
                 <div className="status-tag">온라인 ― {onlineUsers.length}</div>
