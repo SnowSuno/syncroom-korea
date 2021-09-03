3a20f5beef1044fcca30daf83761d551b356d659
SnowSuno <percy3368@gmail.com>
2021-09-03 17:12:26 +0900

    fix : apply react-transition-group to member list

--- src/components/browser/RoomTile/MemberList.tsx
+++ src/components/browser/RoomTile/MemberList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {CSSGrid, enterExitStyle} from "react-stonecutter";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 import {MemberType} from "../../../common/classes/Member";
 import MemberProfile from "./Member";
@@ -10,36 +10,24 @@ interface MemberListProps {
 }
 
 function MemberList({members}: MemberListProps) {
-    const animationStyle = enterExitStyle.fromTop;
-
     return (
         <div className="MemberList">
             <div className="line">
                 <div/><div/><div/><div/><div/>
             </div>
             <div className="MemberContainer">
-                <CSSGrid
-                    component="ul"
-                    columns={1}
-                    lengthUnit="rem"
-                    columnWidth={17.2}
-                    itemHeight={3.08}
-                    duration={300}
-                    {...animationStyle}
-                >
-                    {
-                        members.map((member, index) => {
-                            const key = (member.icon === "-1")
-                                ? index
-                                : member.nickname + member.icon;
-                            return (
-                                <li key={key}>
-                                    <MemberProfile member={member}/>
-                                </li>
-                            )
-                        })
-                    }
-                </CSSGrid>
+                <TransitionGroup>
+                    {members.map((member, index) => {
+                        const key = (member.icon === "-1")
+                            ? index
+                            : member.nickname + member.icon;
+                        return (
+                            <CSSTransition key={key} timeout={200} classNames="wrap">
+                                <MemberProfile member={member}/>
+                            </CSSTransition>
+                        )
+                    })}
+                </TransitionGroup>
             </div>
         </div>
     );
