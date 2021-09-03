1bf377dafb4a8116848acd0a5602000b85583bb8
SnowSuno <percy3368@gmail.com>
2021-09-04 01:29:08 +0900

    feat : add user delete button

--- src/components/browser/UserList/OnlineUser.tsx
+++ src/components/browser/UserList/OnlineUser.tsx
@@ -2,6 +2,9 @@ import React, {useCallback} from "react";
 
 import {useDispatch} from "react-redux";
 import {closeSidebar} from "../../../modules/sidebar";
+import {deleteUser} from "../../../modules/user";
+
+import {ReactComponent as Delete} from "../../../resource/img/icon/x.svg";
 
 interface OnlineUserProps {
     userName: string;
@@ -20,13 +23,24 @@ function OnlineUser({userName, roomId, isActive}: OnlineUserProps) {
         }
     }, [roomId, dispatch, isActive]);
 
+    const deleteSelf = useCallback(() => {
+        dispatch(deleteUser(userName));
+    }, [dispatch, userName]);
+
 
     return (
         <div className="Online User" onClick={findUserRoom}>
             <svg className="indicator" viewBox="0 0 2 2">
                 <circle cx="1" cy="1" r="1"/>
             </svg>
-            {userName}
+            <span>{userName}</span>
+            <button
+                className="delete"
+                onClick={deleteSelf}
+                disabled={!isActive}
+            >
+                <Delete />
+            </button>
         </div>
     );
 }
@@ -43,10 +57,6 @@ const moveTo = (roomId: number) => {
             {boxShadow: "0 0 0.8rem rgba(127, 127, 127, 0.1)"},
         ], 2000);
     }
-
-
-
-
 }
 
 export default OnlineUser;
