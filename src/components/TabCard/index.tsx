1e9bdc26ffcc56e7dbc915163f93e7777f4ee020
SnowSuno <snowsuno.dev@gmail.com>
2022-01-26 13:24:18 +0900

    feat : add notice items

--- src/components/TabCard/index.tsx
+++ src/components/TabCard/index.tsx
@@ -2,13 +2,17 @@ import React, {ReactNode} from "react";
 import "./style.scss";
 
 interface TabCardProps {
+    title?: string;
+    tag?: string
     children?: ReactNode;
 }
 
-function TabCard({children}: TabCardProps) {
+function TabCard({title, tag, children}: TabCardProps) {
 
     return (
         <div className="TabCard">
+            <div className="tag">{tag}</div>
+            <div className="title">{title}</div>
             {children}
         </div>
     )
