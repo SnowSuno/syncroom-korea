e81b7b63b1314778de2b9342e24782b9e6475564
Soonho Kwon <percy3368@gmail.com>
2023-01-03 21:27:05 +0900

    feat: Add tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './style.scss';
 
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
