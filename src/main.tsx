af067c696e2c9f31e770659e2e0f65100133f4eb
Soonho Kwon <percy3368@gmail.com>
2023-10-15 18:53:26 +0900

    feat: Update `README.md`

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
 
-import {createStore, applyMiddleware} from "redux";
-import {Provider} from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import rootReducer from "./modules";
 import ReduxThunk from "redux-thunk";
-import {persistStore} from "redux-persist";
-import {PersistGate} from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
-import Home from "./routes/Home";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from "@/routes/Home";
 import Join from "./routes/Join";
 import Notice from "./routes/Notice";
 import Donate from "./routes/Donate";
@@ -18,31 +23,27 @@ import NotFound from "./routes/NotFound";
 import Modal from "./components/Modal";
 import Sidebar from "./components/Sidebar";
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(ReduxThunk)
-);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 const persistor = persistStore(store);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/join" component={Join}/>
-                        <Route path="/notice" component={Notice}/>
-                        <Route path="/buymeacoffee" component={Donate}/>
-                        <Route exact path="/notfound" component={NotFound} />
-                        <Route component={() => <Redirect to="/notfound"/>}/>
-                    </Switch>
-                    <Modal />
-                    <Sidebar />
-                </Router>
-
-            </PersistGate>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/join" component={Join} />
+            <Route path="/notice" component={Notice} />
+            <Route path="/buymeacoffee" component={Donate} />
+            <Route exact path="/notfound" component={NotFound} />
+            <Route component={() => <Redirect to="/notfound" />} />
+          </Switch>
+          <Modal />
+          <Sidebar />
+        </Router>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
