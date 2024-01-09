e7e9b61f4a0a32161d0ee75235fa22a19c4aceb3
Soonho Kwon <percy3368@gmail.com>
2024-01-09 12:35:32 +0900

    :wrench: Setup `twin.macro`

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,37 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
+  plugins: [
+    svgr(),
+    react({
+      babel: {
+        plugins: [
+          "babel-plugin-macros",
+          [
+            "@emotion/babel-plugin-jsx-pragmatic",
+            {
+              export: "jsx",
+              import: "__cssprop",
+              module: "@emotion/react",
+            },
+          ],
+          [
+            "@babel/plugin-transform-react-jsx",
+            { pragma: "__cssprop" },
+            "twin.macro",
+          ],
+        ],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
