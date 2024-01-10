f0793a841523c4ad7c71510747a7cf19244cfa6e
Soonho Kwon <percy3368@gmail.com>
2024-01-11 02:08:52 +0900

    :ambulance: Fix unique id generation logic

--- src/schema/member.ts
+++ src/schema/member.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 import { iconSchema } from "@/schema/icon";
 
-export const memberSchema = z.object({
+const generateId = <T extends { userId: string; nsgmMemberId: number }>(
+  member: T,
+) => ({
+  ...member,
+  id: `${member.userId}-${member.nsgmMemberId}`,
+});
+
+const baseMemberSchema = z.object({
   userId: z.string(),
   nickname: z.string(),
   nsgmMemberId: z.coerce.number(),
@@ -9,10 +16,14 @@ export const memberSchema = z.object({
   favorite: z.boolean(),
 });
 
+export const memberSchema = baseMemberSchema.transform(generateId);
+
 export type Member = z.infer<typeof memberSchema>;
 
-export const creatorSchema = memberSchema.extend({
-  idProvider: z.enum(["ymid-jp", "ymid-kr", ""]),
-});
+export const creatorSchema = baseMemberSchema
+  .extend({
+    idProvider: z.enum(["ymid-jp", "ymid-kr", ""]),
+  })
+  .transform(generateId);
 
 export type Creator = z.infer<typeof creatorSchema>;
