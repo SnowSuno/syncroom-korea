cf56ce9aa4fcec3eba1b66affd98ca30f9b96adc
Soonho Kwon <percy3368@gmail.com>
2024-02-12 17:36:36 +0900

    :lipstick: Configure base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,37 @@ module.exports = {
         "2xl": "1400px",
       },
     },
-    extend: {
-      colors: {
-        theme: "#6DC2B6",
+    colors: {
+      white: "#ffffff",
+      gray: {
+        50: "#f9fafb",
+        100: "#f2f4f6",
+        200: "#e5e8eb",
+        300: "#d1d6db",
+        400: "#b0b8c1",
+        500: "#8b95a1",
+        600: "#6b7684",
+        700: "#4e5968",
+        800: "#333d4b",
+        900: "#191f28",
       },
+
+      primary: {
+        default: "#26a797",
+      },
+    },
+    fontWeight: {
+      thin: "100",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      bold: "700",
+    },
+    extend: {
       spacing: {
         fold: "theme(spacing[16])",
         expand: "theme(spacing[80])",
+        profile: "theme(spacing[10])",
       },
       keyframes: {
         "accordion-down": {
@@ -34,7 +58,7 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       gridTemplateColumns: {
-        "auto-fit": "repeat(auto-fit, minmax(theme(spacing[80]), 1fr))",
+        "auto-fit": "repeat(auto-fit, minmax(theme(spacing[96]), 1fr))",
       },
     },
   },
